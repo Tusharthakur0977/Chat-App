c9699b5c4bd5438b208b833dbbcf4773386ad974
unknown <tusharthakur0977@gmail.com>
2022-09-15 16:40:42 +0530

    Revert "React JS Chat App"
    This reverts commit 4e7033edc9b68c58314bf09e041c7ce0d44a1cbb.

--- frontend/src/components/ChatUi/ChatBox.js
+++
@@ -1,8 +0,0 @@
-import { Box } from "@mui/material";
-import React from "react";
-
-const ChatBox = () => {
-  return <Box sx={{ minWidth: "64%", minHeight: "89vh" }}>Chat Box</Box>;
-};
-
-export default ChatBox;
